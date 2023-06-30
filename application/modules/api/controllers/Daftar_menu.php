0d7cd19e3e307fff6432a5559a9ac0a532c76cc4
Airmax21 <iqbal.agyan@gmail.com>
2023-06-30 20:34:35 +0700

    Update Daftar_menu.php

--- application/modules/api/controllers/Daftar_menu.php
+++ application/modules/api/controllers/Daftar_menu.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Favorite_menu extends MY_Controller
+class Daftar_menu extends MY_Controller
 {
     public function __construct()
     {
